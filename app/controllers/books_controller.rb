7edf60873000825564577df0ba432bdc96d5ae59
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2018-11-20 10:42:11 -0600

    new book modal

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
+
+  # def new
+  #   @book = Book.new
+  # end
 
   def create
     @book = Book.new(book_params)
