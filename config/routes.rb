dab08d3f69c80cbc23126809ff76040324a5f4c2
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2018-11-24 23:17:57 -0600

    added name and last_name to user forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  # devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   # # resources :users
   # # resources :devise
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
