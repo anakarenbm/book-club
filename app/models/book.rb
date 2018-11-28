f8b40170dc73f8620d57f9dc156b9c7da1d409a5
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2018-11-28 16:12:36 -0600

    user can see book reviews added methods for user_initial and username

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,18 @@
 class Book < ApplicationRecord
   belongs_to :genre
   mount_uploader :picture, PictureUploader
+
+  def avgRating
+    sum = 0
+    count = 0
+    Review.where(book: self).find_each do |review|
+      sum += review.stars
+      count += 1
+    end
+    if count > 0
+      return sum/count
+    end
+    return 0
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :book_lists, as: :ownerable
+
+  def user_initial
+    return initial_to_caps(name || email)
+  end
+
+  def initial_to_caps(username)
+    username[0].upcase
+  end
+
+  def username
+    if last_name.nil?
+      return (name || email)
+    elsif !name.nil?
+      return name + " " + last_name
+    end
+    return email
+  end
+
 end
