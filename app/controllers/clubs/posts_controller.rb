8ffdf698ba37f46b71a48babf3ca3783ca3ca3e7
Ana Karen Beltrán <anakarenbmak@yahoo.com>
2018-11-28 20:58:20 -0600

    authors of posts can edit the content of posts

--- app/controllers/clubs/posts_controller.rb
+++ app/controllers/clubs/posts_controller.rb
@@ -1,85 +1,56 @@
 module Clubs
-class PostsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  class PostsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
-  end
+    def create
+      @club = Club.find(params[:club_id])
+      @post = Post.new(post_params.merge(default_params))
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
-  # POST /posts.json
-  def create
-    @club = Club.find(params[:club_id])
-    @post = Post.new(post_params.merge(default_params))
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @club, notice: 'Post was successfully created.' }
-        # format.json { render :show, status: :created, location: @post }
-      else
-        # format.html { render :new }
-        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to @club, notice: 'Post was successfully created.' }
+        else
+          format.html { redirect_to @club, notice: 'Error while creating post.' }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        # format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        # format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      # format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      # format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
+    def update
+      @club = Club.find(params[:club_id])
       @post = Post.find(params[:id])
+
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @club, notice: 'Post was successfully updated.' }
+        else
+          format.html { redirect_to @club, notice: 'Error while updating post.' }
+        end
+      end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:content, :club_id)
+    def destroy
+      @club = Club.find(params[:club_id])
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to @club, notice: 'Post was successfully destroyed.' }
+      end
     end
 
-    def default_params
-      {
-        club: @club,
-        user: current_user
-      }
-    end
-end
+    private
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.require(:post).permit(:content, :club_id)
+      end
+
+      def default_params
+        {
+          club: @club,
+          user: current_user
+        }
+      end
+  end
 end
